86de3ff48a6860dbe864551887658c51477e1a74
CeIIardoor <cellardoorweb@gmail.com>
2022-03-30 02:01:04 +0000

    ez

--- Python Functions, Files, and Dictionaries/Week5/course_2_project.py
+++ Python Functions, Files, and Dictionaries/Week5/course_2_project.py
@@ -1,9 +1,6 @@
 # function called strip_punctuation which takes one parameter, a string which represents a word, and removes characters considered punctuation from everywhere in the word. (Hint: remember the .replace() method for strings.)
 
 
-from numpy import negative
-
-
 punctuation_chars = ["'", '"', ",", ".", "!", ":", ";", '#', '@']
 
 def strip_punctuation(word):
@@ -12,6 +9,8 @@ def strip_punctuation(word):
         if char in punctuation:
             word = word.replace(char, "")
     return word
+
+
 #Next, copy in your strip_punctuation function and define a function called get_pos which takes one parameter, a string which represents one or more sentences, and calculates how many words in the string are considered positive words. Use the list, positive_words to determine what words will count as positive. The function should return a positive integer - how many occurrences there are of positive words in the text. Note that all of the words in positive_words are lower cased, so you’ll need to convert all the words in the input string to lower case as well.
 
 positive_words = []
