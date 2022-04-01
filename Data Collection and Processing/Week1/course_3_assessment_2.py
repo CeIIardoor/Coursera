be152288ec1c5d730e2eb5d8f8bbfabaac858b51
CeIIardoor <cellardoorweb@gmail.com>
2022-04-01 17:35:10 +0000

    js

--- Data Collection and Processing/Week1/course_3_assessment_2.py
+++ Data Collection and Processing/Week1/course_3_assessment_2.py
@@ -27,3 +27,23 @@ lst2 = [x * 2 for x in lst]
 
 students = [('Tommy', 95), ('Linda', 63), ('Carl', 70), ('Bob', 100), ('Raymond', 50), ('Sue', 75)]
 
+passed = [x[0] for x in students if x[1] >= 70]
+
+#Write code using zip and filter so that these lists (l1 and l2) are combined into one big list and assigned to the variable opposites if they are both longer than 3 characters each.
+
+l1 = ['left', 'up', 'front']
+l2 = ['right', 'down', 'back']
+
+opposites = list(filter(lambda x: len(x[0]) > 3 and len(x[1]) > 3, zip(l1, l2)))
+
+#Below, we have provided a species list and a population list. Use zip to combine these lists into one list of tuples called pop_info. From this list, create a new list called endangered that contains the names of species whose populations are below 2500.
+
+
+species = ['golden retriever', 'white tailed deer', 'black rhino', 'brown squirrel', 'field mouse', 'orangutan', 'sumatran elephant', 'rainbow trout', 'black bear', 'blue whale', 'water moccasin', 'giant panda', 'green turtle', 'blue jay', 'japanese beetle']
+
+population = [10000, 90000, 1000, 2000000, 500000, 500, 1200, 8000, 12000, 2300, 7500, 100, 1800, 9500, 125000]
+
+pop_info = list(zip(species, population))
+endangered = [x[0] for x in pop_info if x[1] < 2500]
+
+#
